a320689480ebcc864d981de1648b6106db2bd88c
Mahmud Affa <affamahmud7@gmail.com>
2022-11-17 07:41:04 -0800

    modified all commits

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,21 @@
 #include "dog.h"
+#include "stdlib.h"
 
 /**
  * init_dog-initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner of the dog.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  *
  * Return: no return.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- * struct dog- a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog-a dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  *
- * Description: Longer description
+ * Description: data structure for dog
  */
 struct dog
 {
@@ -15,17 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t-typedef for struct dog
- */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name. float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
