5d410ac6107198b878848818fcace2ac3f7f8291
codewithMahmouda <affamahmud7@gmail.com>
2023-09-11 13:11:41 -0700

    Print alphabet in lowercase except 2 characters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
