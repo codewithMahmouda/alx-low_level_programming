469abad300f665c2a7d2e295d2f4115eccdc38ba
codewithMahmouda <affamahmud7@gmail.com>
2023-09-12 15:37:32 -0700

    Print combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int n, x, y;
+
+	for (n = '0'; n < '8'; n++)
+	{
+		for (x = '1'; x < '9'; x++)
+		{
+			for (y = '2'; y <= '9'; y++)
+			{
+				if (n < x && x < y)
+				{
+					putchar(n);
+					putchar(x);
+					putchar(y);
+					if (n < '7' || x < '8' || y <= '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
