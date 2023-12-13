ef7ca95ba6c0d9564eff577772632077f320d44c
codewithMahmouda <affamahmud7@gmail.com>
2023-12-13 01:09:38 -0800

    Update function for malloc and exit status

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,20 @@
+#include "main.h"
 #include <stdlib.h>
+
 /**
-  * malloc_checked - allocates memory using malloc.
-  * @b: size of memory to be allocated.
-  *
-  * Return: pointer to allocated memory.
-  */
+ * malloc_checked - This is a function that allocates memory using
+ * malloc.
+ * @b: memory size to be allocated.
+ *
+ * Return: Pointer to allocated memory.
+ */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *a = malloc(b);
 
-	if (p == NULL)
+	if (a == NULL)
 		exit(98);
 
-	return (p);
+	return (a);
 }
