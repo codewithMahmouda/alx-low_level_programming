12259fc80ec0467c2d577ced7bf7ee41df12b762
codewithMahmouda <affamahmud7@gmail.com>
2024-04-23 00:34:24 +0000

    Hot fix!

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,12 @@
 
 void free_list(list_t *head)
 {
-	list_t *current_node;
+	list_t *temp = head;
 
-	while ((current_node = head) != NULL)
+	while (temp != NULL)
 	{
-		head = head->next;
-		free(current_node->str);
-		free(current_node);
+		temp = temp->next;
+		free(head);
+		head = temp;
 	}
 }
